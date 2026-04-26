8a0ea4907a6f4d315cfd6f7247e2969acf31d5b8
sherry <258342076+sherry-gcp@users.noreply.github.com>
2026-04-26 12:28:01 +0800

    infra: output for wif and service url

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,4 +1,8 @@
-output "cloud_run_url" {
-    description = "The live URL of the Store Locatore Discovery API"
-    value = google_cloud_run_v2_service.api_server.uri
+output "service_url" {
+  value = google_cloud_run_v2_service.api_server.uri
+}
+
+output "workload_identity_provider" {
+  value       = "projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/providers/${google_iam_workload_identity_pool_provider.github_provider.workload_identity_pool_provider_id}"
+  description = "The Workload Identity Provider ID for GitHub Actions"
 }
