d2a833b767e3136948fdf5daa757fd3e97100c60
sherry <258342076+sherry-gcp@users.noreply.github.com>
2026-05-02 19:06:40 +0800

    feat: implement GCS logic on backend

--- api/config.py
+++ api/config.py
@@ -1,10 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     PROJECT_NAME: str = "SRE Portfolio"
     DATA_PATH: str = "api/data/projects.json"
-    ASSETS_BUCKET: str = "portfolio-assets-sherry-dev"
+    ASSETS_BUCKET: str = "portfolio-assets-sre-portfoliox"
 
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
 
 settings = Settings()
